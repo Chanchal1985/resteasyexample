4ea755ab90cfc3b6d595e37f590824ccdb656792
chanchal <chanchalnerkar@gmail.com>
2018-08-12 22:35:18 +0200

    Checkin after tomcat integration

--- src/main/java/com/resteasy/controller/MessageRestService.java
+++ src/main/java/com/resteasy/controller/MessageRestService.java
@@ -2,6 +2,7 @@ package com.resteasy.controller;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
 @Path("/message")
@@ -9,7 +10,14 @@ public class MessageRestService {
 
     @GET
     @Path("/{param}")
-    public Response printMessage(String message){
+    public Response printMessage(@PathParam("param") String message){
         return Response.status(Response.Status.OK).entity("Restful Example "+ message).build();
+    }
+
+    @GET
+    @Path("/somemethod")
+    public Response someMethod(){
+        Response helloWorld = Response.status(Response.Status.OK).entity("HelloWorld").build();
+        return helloWorld;
     }
 }
